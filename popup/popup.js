c720c850ec7e5bd39bd9bdf376bb849f10b11adf
Matsew-uwu <randriantsoamatthieu@gmail.com>
2022-12-17 05:17:29 +0100

    Handle extension launch elsewhere ClarolineConnect

--- popup/popup.js
+++ popup/popup.js
@@ -1,44 +1,52 @@
-
 chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  let response = await chrome.tabs.sendMessage(tab.id, { type: "getNodesInformation" });
+  if (!tab.url.includes('https://clarolineconnect.univ-lyon1.fr/')) {
+    displayErrorPopup();
+  } else {
+    document.getElementById('instruction-text').innerHTML = "Sélectionner un fichier pour le visionner.";
+    let response = await chrome.tabs.sendMessage(tab.id, { type: "getNodesInformation" });
 
-  const nodes = response.nodesInformation;
+    const nodes = response.nodesInformation;
 
-  let fileList = document.getElementById("file-list");
+    let fileList = document.getElementById("file-list");
 
-  for (let i = 0; i < nodes.length; i++) {
-    let node = nodes[i];
-    const listGroupItem = document.createElement('li');
-    listGroupItem.id = node.id;
-    listGroupItem.classList.add('list-group-item', 'list-group-item-action');
+    for (let i = 0; i < nodes.length; i++) {
+      let node = nodes[i];
+      const listGroupItem = document.createElement('li');
+      listGroupItem.id = node.id;
+      listGroupItem.classList.add('list-group-item', 'list-group-item-action');
 
-    const container = document.createElement('div');
-    container.classList.add('list-group-item__container');
+      const container = document.createElement('div');
+      container.classList.add('list-group-item__container');
 
-    const text = document.createElement('div');
-    text.classList.add('list-group-item__text');
-    text.textContent = node.title;
+      const text = document.createElement('div');
+      text.classList.add('list-group-item__text');
+      text.textContent = node.title;
 
-    const icon = document.createElement('div');
-    icon.classList.add('list-group-item__icon');
+      const icon = document.createElement('div');
+      icon.classList.add('list-group-item__icon');
 
-    const iconElem = document.createElement('i');
-    iconElem.classList.add('material-icons');
-    // iconElem.textContent = 'check';
+      const iconElem = document.createElement('i');
+      iconElem.classList.add('material-icons');
+      // iconElem.textContent = 'check';
 
-    icon.appendChild(iconElem);
-    container.appendChild(text);
-    container.appendChild(icon);
-    listGroupItem.appendChild(container);
+      icon.appendChild(iconElem);
+      container.appendChild(text);
+      container.appendChild(icon);
+      listGroupItem.appendChild(container);
 
-    listGroupItem.addEventListener('click', function () {
-      let newURL = `https://clarolineconnect.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/${node.id}`;
-      chrome.tabs.create({ url: newURL });
-    });
+      listGroupItem.addEventListener('click', function () {
+        let newURL = `https://clarolineconnect.univ-lyon1.fr/clarolinepdfplayerbundle/pdf/${node.id}`;
+        chrome.tabs.create({ url: newURL });
+      });
 
-    fileList.appendChild(listGroupItem);
+      fileList.appendChild(listGroupItem);
+    }
   }
-
 });
+
+const displayErrorPopup = () => {
+  const instruction = document.getElementById('instruction-text');
+  instruction.innerHTML = 'Cette extension fonctionne uniquement sur <a href="clarolineconnect.univ-lyon1.fr/">Claroline Connect</a>.';
+};
